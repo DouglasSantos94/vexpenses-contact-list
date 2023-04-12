7368ec5f6e7aa6e10402c599823534da55fd32cd
Douglas Santos <dgstos94@gmail.com>
2023-04-12 19:52:01 -0300

    feature: create base card component

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./style";
+
+export const Contact = () => {
+  return (
+    <C.Card>
+      <C.CardTitle>Teste</C.CardTitle>
+      <C.ContactInfo>
+        <p>Nome: Teste</p>
+        <p>Telefone: Teste</p>
+      </C.ContactInfo>
+    </C.Card>
+  );
+}
---
+++ src/pages/Contact/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
+  margin-bottom: 5px;
+`;
+
+export const CardTitle = styled.h2`
+  font-size: 36px;
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+`;
+
+
+export const ContactInfo = styled.div`
+  width: 70%;
+  font-size: 24px;
+`;
