2e57bd9f764a6d50512edaeefe09dbea275b4a0a
Douglas Santos <dgstos94@gmail.com>
2023-04-13 19:34:55 -0300

    feature: create ContactInfo component and route

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import { Home } from "./pages/Home";
+import { ContactInfo } from "./pages/Contact/ContactInfo";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "contact",
+    element: <ContactInfo />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
