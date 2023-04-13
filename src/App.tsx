2ba9ac07820420141f988b76cabf630f8e42acf8
Douglas Santos <dgstos94@gmail.com>
2023-04-13 10:20:50 -0300

    chore: install json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import { Contact } from "./pages/Contact";
-import { ContactList } from "./pages/ContactList/style";
+import { ContactList } from "./pages/ContactList";
 
 function App() {
 
   return (
     <div className="App">
-      <ContactList>
-        {Array.from({length: 10}, (x, i) => (<li key={i}>
-          <Contact />
-        </li>))}
-      </ContactList>
+      <ContactList />
     </div>
   );
 }
