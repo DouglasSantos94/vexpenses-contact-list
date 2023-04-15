95d068e8fd6233e2c0492942bfd7b440da456a14
Douglas Santos <dgstos94@gmail.com>
2023-04-15 12:13:03 -0300

    feature: create add contact route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import { Home } from "./pages/Home";
 import { ContactDetail } from "./pages/Contact/ContactDetail";
+import { AddContact } from "./pages/Contact/AddContact";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: "contact/:id",
         element: <ContactDetail />
+      },
+      {
+        path: "add-contact",
+        element: <AddContact />
       }
     ]
   }
