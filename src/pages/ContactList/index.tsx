be8c16f0ada77674be14e413086c7f6dc3aa2867
Douglas Santos <dgstos94@gmail.com>
2023-04-16 16:00:12 -0300

    feature: create list sections by first letter

--- src/pages/ContactList/index.tsx
+++ src/pages/ContactList/index.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { StyledContactList } from "./style";
 import { ContactCard, Contact } from "../Contact";
 
-export interface IContactListProps {
-  contacts: Contact[],
-  handleDelete: (id: string) => void
-}
+export const ContactList = () => {
 
-export const ContactList: React.FC<IContactListProps> = ({contacts, handleDelete}: IContactListProps) => {
-  console.log(contacts)
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  const handleDelete = (id: string) => {
+    console.log(id)
+    axios.delete(`http://localhost:3000/contacts/${id}`)
+      .then(r => console.log(r.status))
+      .finally(() => window.location.reload());
+  }
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.value.toUpperCase();
+    setContacts(contacts.filter((contact: Contact) => contact.name.toUpperCase().startsWith(name)));
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/contacts")
+      .then(({data})=> setContacts(data));
+  }, [])
+
   return (
-    <StyledContactList>
-      {contacts.map(({id, name, addresses, phoneNumbers}: Contact, i) => (
-        <ContactCard id={id} name={name} addresses={addresses} phoneNumbers={phoneNumbers} handleDelete={handleDelete} key={i}/>
-      ))}
-    </StyledContactList>
+    <>
+      <input type="text" onChange={handleSearch} placeholder="BUscar contato" />
+      <StyledContactList>
+        {letters.map(
+          (letter, i) => (
+            <div key={i}>
+              <p>{letter}</p>
+              {contacts.filter(contact => contact.name[0] == letter).map(({id, name, addresses, phoneNumbers}: Contact, i) => 
+                <ContactCard id={id} name={name} addresses={addresses} phoneNumbers={phoneNumbers} handleDelete={handleDelete} key={i}/>
+              )}
+            </div>
+          ))}
+      </StyledContactList>
+    </>
   )
 }
