d72a469063a76ec356cd3b549d836a262a65c6e6
Douglas Santos <dgstos94@gmail.com>
2023-04-16 19:32:41 -0300

    feature: create Form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FormProvider, UseFormProps, UseFormReturn } from "react-hook-form";
+import { Contact } from "../../pages/Contact";
+import { ContactAddressForm } from "../../pages/Contact/AddContact/AddressForm";
+import { PhoneNumberForm } from "../../pages/Contact/AddContact/PhoneNumberForm";
+import { NameInput } from "../../pages/Contact/AddContact";
+
+export interface IFormProps {
+  isEdit: boolean,
+  form: UseFormReturn<Contact, UseFormProps>,
+  submitForm: (form: Contact) => void
+}
+
+export const Form = ({isEdit, form, submitForm}: IFormProps) => {
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(submitForm)}>
+        <section>
+          <NameInput />
+        </section>
+        <section>
+          <h2>Endereços</h2>
+          <ContactAddressForm />
+        </section>
+        <PhoneNumberForm />
+        <button type="submit">{`Salvar ${isEdit ? "alterações" : "contato"}`}</button>
+      </form>
+    </FormProvider>
+)
+}
