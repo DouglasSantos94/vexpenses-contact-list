62099cb6a3d4b69411e839db59e2e94bc3bcb66d
Douglas Santos <dgstos94@gmail.com>
2023-04-19 18:50:53 -0300

    style: create Reset global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Reset from "./styles/Reset";
 
 function App() {
 
   return (
     <div className="App">
+      <Reset />
       <Outlet />
     </div>
   );
