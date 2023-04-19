1e6ee19f32b20ba3b5c26c227f98fd4532eb7907
Douglas Santos <dgstos94@gmail.com>
2023-04-18 22:49:42 -0300

    refactor: get contact data from useParams instead state

--- src/pages/Contact/ContactDetail/index.tsx
+++ src/pages/Contact/ContactDetail/index.tsx
@@ -1,23 +1,50 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { Address, PhoneNumber } from "..";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { PhoneNumber } from "../../../types/phoneNumber";
+import { Address } from "../../../types/address";
+import axios from "axios";
 
 export const ContactDetail = () => {
-  const location = useLocation();
-  const {id, name, addresses, phoneNumbers} = location.state;
+  const [ contact, setContact ] = useState({
+    id: "",
+    name: "",
+    addresses: [],
+    phoneNumbers: []
+  });
+
+  const { id } = useParams();
+
+  const handleDelete = (id: string) => {
+    axios.delete(`http://localhost:3000/contacts/${id}`)
+      .then(r => console.log(r.status))
+      .finally(() => window.location.reload());
+  }
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/contacts/${id}`)
+      .then(({data}) => setContact(data))
+  }, [id]);
+
   return (
     <>
-      <h1>Nome: {name}</h1>
-      <h2>Telefones:</h2>
-      {phoneNumbers.map((phoneNumber: PhoneNumber, i: number) => (
-        <p key={i}>Telefone {i+1}: {phoneNumber.number}</p>
-      ))}
-      <h2>Endereços</h2>
-      {addresses.map((address: Address, i: number) => (
-        <p key={i}>Endereço {i+1}: {address.street}</p>
-      ))}
-      <Link to={`/edit/${id}`} state={location.state}>Editar contato</Link>
+      {
+        contact && id && (
+          <>
+            <h1>Nome: {contact.name}</h1>
+            <h2>Telefones:</h2>
+            {contact.phoneNumbers.map((phoneNumber: PhoneNumber, i: number) => (
+              <p key={i}>Telefone {i+1}: {phoneNumber.number}</p>
+            ))}
+            <h2>Endereços</h2>
+            {contact.addresses.map((address: Address, i: number) => (
+              <p key={i}>Endereço {i+1}: {address.street}</p>
+            ))}
+            <Link to={`/edit/${id}`}>Editar contato</Link>
+            <button onClick={() => handleDelete(id)}>Excluir</button>
+          </>
+        )
+      }
     </>
   )
 }
