7240cbe08f9d424f81c981c0bc9dd17d95541d80
Douglas Santos <dgstos94@gmail.com>
2023-04-19 14:46:41 -0300

    style: adjust ContactCard

--- src/pages/Contact/style.ts
+++ src/pages/Contact/style.ts
@@ -1,16 +1,27 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
+  font-family: "Roboto", sans-serif;
   width: 100%;
+  padding: 20px;
   display: flex;
   flex-direction: column;
+  align-items: center;
   border-radius: 8px;
-  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
-  margin-bottom: 5px;
+  padding: 10px;
+  box-sizing: border-box;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.2);
+  margin-bottom: 10px;
+
+  @media (max-width: 600px)
+  {
+    width: 100%;
+  }
+  
 `;
 
 export const CardTitle = styled.h2`
-  font-size: 36px;
+  font-size: 30px;
   margin: 0;
   padding: 0;
   font-weight: normal;
@@ -18,6 +29,6 @@ export const CardTitle = styled.h2`
 
 
 export const ContactInfo = styled.div`
-  width: 70%;
+  width: 50%;
   font-size: 24px;
 `;
