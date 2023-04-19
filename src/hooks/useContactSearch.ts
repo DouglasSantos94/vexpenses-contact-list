d8e19801b52ab9577df7ba202a5fdd360dcc3662
Douglas Santos <dgstos94@gmail.com>
2023-04-18 23:46:33 -0300

    refactor: remove unused methods

--- src/hooks/useContactSearch.ts
+++ src/hooks/useContactSearch.ts
@@ -5,56 +5,33 @@ export const useContactSearch = (contacts: Contact[], contactSearchRef: HTMLInpu
 
   const [searchedContactName, setSearchedContactName] = useState("");
   const [suggestions, setSuggestions] = useState<Contact[]>([]);
-  const [selectedSuggestion, setSelectedSuggestion] = useState("");
-  const [activeSuggestion, setActiveSuggestion] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value !== "") {
       const filteredSuggestions = contacts?.filter((contact) => {
-        const value = e.target.value.toUpperCase()
-        const name = contact.name.toUpperCase()
+        const value = e.target.value.toUpperCase();
+        const name = contact.name.toUpperCase();
 
-        return value && name.startsWith(value) && name !== value
+        return value && name.startsWith(value) && name !== value;
       })
-      setSearchedContactName(e.target.value)
-      setSuggestions(filteredSuggestions)
+      setSearchedContactName(e.target.value);
+      setSuggestions(filteredSuggestions);
+      setShowSuggestions(true);
+    } else {
+      setShowSuggestions(false);
     }
-  }
-
-  const handleKeyDown = (
-    event: React.KeyboardEvent<HTMLInputElement>
-  ): void => {
-    if (event.key === "ArrowDown" && activeSuggestion < suggestions.length) {
-      setActiveSuggestion(activeSuggestion + 1)
-    } else if (event.key === "ArrowUp" && activeSuggestion > 1) {
-      setActiveSuggestion(activeSuggestion - 1)
-    } else if (event.key === "Enter") {
-      setSearchedContactName(suggestions[activeSuggestion - 1].name)
-      setSelectedSuggestion(suggestions[activeSuggestion - 1].name)
-      setSuggestions([])
-      setActiveSuggestion(0)
-    }
-  }
-
-  const handleClick = (value: string) => {
-    setSelectedSuggestion(value)
-    setSearchedContactName(value)
-    setSuggestions([])
-    setActiveSuggestion(0)
   }
 
   useEffect(() => {
     if(contactSearchRef) contactSearchRef.focus();
-  }, [])
+  }, []);
 
 
   return {
     searchedContactName,
     suggestions,
-    selectedSuggestion,
-    activeSuggestion,
     handleSearch,
-    handleKeyDown,
-    handleClick,
+    showSuggestions
   }
 }
