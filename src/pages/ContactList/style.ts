a7b786ec72df8bd44efc2bccb81182077159e02d
Douglas Santos <dgstos94@gmail.com>
2023-04-19 14:45:13 -0300

    style: adjust contactlist style

--- src/pages/ContactList/style.ts
+++ src/pages/ContactList/style.ts
@@ -3,4 +3,17 @@ import styled from "styled-components";
 export const StyledContactList = styled.ul`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  width: 100%;
+  box-sizing: border-box;
 `;
+
+export const ContactSection = styled.li`
+  margin-top: 20px;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  align-items: center;
+  border-top: solid 1px rgba(0, 0, 0, 0.1);
+`
