d80104a5f831682b6a3d6b0b96293049b8f21d59
Douglas Santos <dgstos94@gmail.com>
2023-04-20 12:53:16 -0300

    refactor: apply styled components to address form

--- src/pages/Contact/AddContact/AddressForm/index.tsx
+++ src/pages/Contact/AddContact/AddressForm/index.tsx
@@ -4,7 +4,9 @@ import { ErrorMessage } from "@hookform/error-message";
 import { viaCep } from "../../../../services/viaCep";
 import { Contact } from "../../../../types/contact";
 import { Address } from "../../../../types/address";
-import { FormInput } from "../../../../components/Form/style";
+import { AddressesTitle, FormInput, InputField } from "../../../../components/Form/style";
+import { AddressWrapper, CepButton, CepField, NewAddressButton, RemoveAddressButton } from "./style";
+
 
 export const ContactAddressForm = () => {
   const form = useFormContext<Contact>();
@@ -54,46 +56,46 @@ export const ContactAddressForm = () => {
   
 
   return (
-    <section>
+    <AddressWrapper>
       {
         controlledFields.map((field, index) => (
           <div key={field.id}>
-            <p>Endereço {index + 1}</p>
-            <button disabled={false} onClick={() => removeAddress(index)}>Remover endereço</button>
-            <div>
-              <div>
+            <AddressesTitle>Endereço {index + 1}</AddressesTitle>
+            <InputField style={{ flexDirection: "row", alignItems: "flex-start" }}>
+              <CepField>
                 <FormInput {...form.register(`addresses.${index}.cep`, { required: "* Campo obrigatório" })} placeholder="CEP" />
                 <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.cep`} />
-              </div>
-              <button type="button" onClick={() => getAddressApi(form.getValues(`addresses.${index}.cep`), index)}>Buscar endereço</button>
-              <div>
-                <FormInput {...form.register(`addresses.${index}.street`, { required: "* Campo obrigatório" })} placeholder="Rua" />
-                <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.street`} />
-              </div>
-              <div>
-                <FormInput {...form.register(`addresses.${index}.number`, { required: "* Campo obrigatório" })} placeholder="Número" />
-                <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.number`} />
-              </div>
-              <div>
-                <FormInput {...form.register(`addresses.${index}.complement`)} placeholder="Complemento" />
-              </div>
-              <div>
-                <FormInput {...form.register(`addresses.${index}.neighborhood`, { required: "* Campo obrigatório" })} placeholder="Bairro" />
-                <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.neighborhood`} />
-              </div>
-              <div>
-                <FormInput {...form.register(`addresses.${index}.city`, { required: "* Campo obrigatório" })} placeholder="Cidade" />
-                <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.city`} />
-              </div>
-              <div>
-                <FormInput {...form.register(`addresses.${index}.state`, { required: "* Campo obrigatório" })} placeholder="Estado" />
-                <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.state`} />
-              </div>
-            </div>
+              </CepField>
+              <CepButton type="button" onClick={() => getAddressApi(form.getValues(`addresses.${index}.cep`), index)}>Buscar endereço</CepButton>
+            </InputField>
+            <InputField>
+              <FormInput {...form.register(`addresses.${index}.street`, { required: "* Campo obrigatório" })} placeholder="Rua" />
+              <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.street`} />
+            </InputField>
+            <InputField>
+              <FormInput {...form.register(`addresses.${index}.number`, { required: "* Campo obrigatório" })} placeholder="Número" />
+              <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.number`} />
+            </InputField>
+            <InputField>
+              <FormInput {...form.register(`addresses.${index}.complement`)} placeholder="Complemento" />
+            </InputField>
+            <InputField>
+              <FormInput {...form.register(`addresses.${index}.neighborhood`, { required: "* Campo obrigatório" })} placeholder="Bairro" />
+              <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.neighborhood`} />
+            </InputField>
+            <InputField>
+              <FormInput {...form.register(`addresses.${index}.city`, { required: "* Campo obrigatório" })} placeholder="Cidade" />
+              <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.city`} />
+            </InputField>
+            <InputField>
+              <FormInput {...form.register(`addresses.${index}.state`, { required: "* Campo obrigatório" })} placeholder="Estado" />
+              <ErrorMessage errors={form.formState.errors} name={`addresses.${index}.state`} />
+            </InputField>
+            {index > 0 && <RemoveAddressButton onClick={() => removeAddress(index)}>Remover endereço</RemoveAddressButton>}
           </div>
         ))
       }
-      <button type="button" onClick={addNewAddress}>Novo endereço</button>
-    </section>
+      <NewAddressButton type="button" onClick={addNewAddress}>Novo endereço</NewAddressButton>
+    </AddressWrapper>
   );
 }
---
+++ src/pages/Contact/AddContact/AddressForm/style.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+
+export const AddressWrapper = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CepField = styled.div`
+  width: 70%;
+`;
+
+export const CepButton = styled.button`
+  width: 30%;
+  height: 30px;
+  color: white;
+  border-radius: 8px;
+  border: none;
+  background-color: #0A7EE3;
+  font-size: 15px;
+`;
+
+export const NewAddressButton = styled.button`
+  width: 20%;
+  height: 30px;
+  color: white;
+  border-radius: 8px;
+  border: none;
+  background-color: #0A7EE3;
+  font-size: 15px;
+  margin-top: 20px;
+`;
+
+export const RemoveAddressButton = styled.button`
+width: 20%;
+height: 30px;
+color: white;
+border-radius: 8px;
+border: none;
+background-color: #DB1414;
+font-size: 15px;
+margin-top: 20px;
+`;
