a1731c3591994f0565d78e35c4dfe0675f470e29
Douglas Santos <dgstos94@gmail.com>
2024-01-22 09:19:28 -0300

    ref: apply prettier

--- src/pages/ContactList/index.tsx
+++ src/pages/ContactList/index.tsx
@@ -1,50 +1,65 @@
 import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
-import { ContactSection, ContactSectionTitle, InputSearchWrapper, StyledContactList, ContactListWrapper, InputSearch, ContactSuggestions, StyledSuggestion } from "./style";
+import {
+  ContactSection,
+  ContactSectionTitle,
+  InputSearchWrapper,
+  StyledContactList,
+  ContactListWrapper,
+  InputSearch,
+  ContactSuggestions,
+  StyledSuggestion,
+} from "./style";
 import { ContactCard } from "../Contact";
 import { Contact } from "../../types/contact";
 import { useContactSearch } from "../../hooks/useContactSearch";
-import { Link } from "react-router-dom";
 import { getContacts } from "../../api";
-
 
 export const ContactList = () => {
   const [contacts, setContacts] = useState<Contact[]>([]);
   const contactSearchRef = useRef<HTMLInputElement>(null);
 
-  const {
-    suggestions,
-    showSuggestions,
-    handleSearch,
-  } = useContactSearch(contacts, contactSearchRef.current);
+  const { suggestions, showSuggestions, handleSearch } = useContactSearch(
+    contacts,
+    contactSearchRef.current
+  );
 
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
   useEffect(() => {
-    getContacts()
-      .then(({data})=> setContacts(data));
+    getContacts().then(({ data }) => setContacts(data));
   }, []);
 
   return (
     <ContactListWrapper>
       <InputSearchWrapper>
-        <InputSearch type="text" onChange={handleSearch} ref={contactSearchRef} placeholder="Buscar contato" />
+        <InputSearch
+          type="text"
+          onChange={handleSearch}
+          ref={contactSearchRef}
+          placeholder="Buscar contato"
+        />
         <ContactSuggestions showSuggestions={showSuggestions}>
-          {suggestions.length > 0 && suggestions.map(({ id, name }: Contact) => <StyledSuggestion key={id} to={`/contact/${id}`}>{name}</StyledSuggestion>)}
+          {suggestions.length > 0 &&
+            suggestions.map(({ id, name }: Contact) => (
+              <StyledSuggestion key={id} to={`/contact/${id}`}>
+                {name}
+              </StyledSuggestion>
+            ))}
         </ContactSuggestions>
       </InputSearchWrapper>
-      
+
       <StyledContactList>
-        {letters.map(
-          (letter, i) => (
-            <ContactSection key={i}>
-              <ContactSectionTitle>{letter}</ContactSectionTitle>
-              {contacts.filter(contact => contact.name[0]?.toUpperCase() == letter).map((contact, i) => 
+        {letters.map((letter, i) => (
+          <ContactSection key={i}>
+            <ContactSectionTitle>{letter}</ContactSectionTitle>
+            {contacts
+              .filter((contact) => contact.name[0]?.toUpperCase() == letter)
+              .map((contact, i) => (
                 <ContactCard contact={contact} key={i} />
-              )}
-            </ContactSection>
-          ))}
+              ))}
+          </ContactSection>
+        ))}
       </StyledContactList>
     </ContactListWrapper>
-  )
-}
+  );
+};
