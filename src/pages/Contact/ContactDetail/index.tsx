99147170a29a0e71bfdab7f4d9841e1951ccb27f
Douglas Santos <dgstos94@gmail.com>
2024-01-27 16:24:52 -0300

    style: adjust COntactDetail card style

--- src/pages/Contact/ContactDetail/index.tsx
+++ src/pages/Contact/ContactDetail/index.tsx
@@ -3,22 +3,23 @@ import { useNavigate, useParams } from "react-router-dom";
 import { PhoneNumber } from "../../../types/phoneNumber";
 import { Address } from "../../../types/address";
 import { deleteContact, getContact } from "../../../api";
-import { ContactDetailCard, 
-  ContactDetailName, 
-  ContactDetailSection, 
-  ContactDetailWrapper, 
-  ContactDetailInfo, 
+import {
+  ContactDetailCard,
+  ContactDetailName,
+  ContactDetailSection,
+  ContactDetailWrapper,
+  ContactDetailInfo,
   ContactDetailButtons,
   EditContact,
-  DeleteContact
+  DeleteContact,
 } from "./style";
 
 export const ContactDetail = () => {
-  const [ contact, setContact ] = useState({
+  const [contact, setContact] = useState({
     id: "",
     name: "",
     addresses: [],
-    phoneNumbers: []
+    phoneNumbers: [],
   });
 
   const { id } = useParams();
@@ -27,44 +28,53 @@ export const ContactDetail = () => {
 
   const handleDelete = (id: string) => {
     deleteContact(id)
-      .then(r => r.status)
-      .catch(status => console.log(status))
+      .then((r) => r.status)
+      .catch((status) => console.log(status))
       .finally(() => navigate("/"));
-  }
+  };
+  console.log(contact);
 
   useEffect(() => {
     getContact(id)
-      .then(({data}) => setContact(data))
-      .catch(e => console.log(e))
+      .then(({ data }) => setContact(data))
+      .catch((e) => console.log(e));
   }, [id]);
 
   return (
     <ContactDetailWrapper>
-      {
-        contact && id && (
-          <ContactDetailCard>
-            <ContactDetailInfo>
-              <ContactDetailName>Nome: {contact.name}</ContactDetailName>
-              <ContactDetailSection>
-                <h2>Telefones</h2>
-                {contact.phoneNumbers.map((phoneNumber: PhoneNumber, i: number) => (
-                  <p key={i}>Telefone {i+1}: {phoneNumber.number}</p>
-                ))}
-              </ContactDetailSection>
-              <ContactDetailSection>
-                <h2>Endereços</h2>
-                {contact.addresses.map((address: Address, i: number) => (
-                  <p key={i}>Endereço {i+1}: {address.street}</p>
-                ))}
-              </ContactDetailSection>
-            </ContactDetailInfo>
-            <ContactDetailButtons>
-              <EditContact to={`/edit/${id}`} state={contact}>Editar contato</EditContact>
-              <DeleteContact onClick={() => handleDelete(id)}>Excluir</DeleteContact>
-            </ContactDetailButtons>
-          </ContactDetailCard>
-        )
-      }
+      {contact && id && (
+        <ContactDetailCard>
+          <ContactDetailInfo>
+            <ContactDetailName>{contact.name}</ContactDetailName>
+            <ContactDetailSection>
+              <h2>Telefones</h2>
+              {contact.phoneNumbers.map(
+                (phoneNumber: PhoneNumber, i: number) => (
+                  <p key={i}>
+                    Telefone {i + 1}: {phoneNumber.number}
+                  </p>
+                )
+              )}
+            </ContactDetailSection>
+            <ContactDetailSection>
+              <h2>Endereços</h2>
+              {contact.addresses.map((address: Address, i: number) => (
+                <p key={i}>
+                  Endereço {i + 1}: {address.street}
+                </p>
+              ))}
+            </ContactDetailSection>
+          </ContactDetailInfo>
+          <ContactDetailButtons>
+            <EditContact to={`/edit/${id}`} state={contact}>
+              Editar contato
+            </EditContact>
+            <DeleteContact onClick={() => handleDelete(id)}>
+              Excluir
+            </DeleteContact>
+          </ContactDetailButtons>
+        </ContactDetailCard>
+      )}
     </ContactDetailWrapper>
-  )
-}
+  );
+};
--- src/pages/Contact/ContactDetail/style.ts
+++ src/pages/Contact/ContactDetail/style.ts
@@ -5,75 +5,84 @@ export const ContactDetailWrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 100px;
-`
+`;
 
 export const ContactDetailCard = styled.div`
+  font-family: "Roboto", sans-serif;
+  width: 60%;
+  padding: 30px 40px;
   display: flex;
   justify-content: space-between;
-  width: 60%;
-  padding: 10px;
+  align-items: center;
   box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2), -1px -1px 3px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
 `;
 
 export const ContactDetailName = styled.h1`
-  font-size: 30px;
-  margin-bottom: 15px;
-`
+  font-size: 25px;
+  position: relative;
+  top: 0;
+  margin-bottom: 25px;
+`;
 
 export const ContactDetailSection = styled.section`
   display: flex;
   flex-direction: column;
-  width: 60%;
+  min-height: 50%;
+  width: 80%;
   margin-bottom: 20px;
 
   h2 {
-    font-size: 25px;
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 13px;
     margin-bottom: 5px;
   }
 `;
 
 export const ContactDetailInfo = styled.div`
-  width: 60%;
+  width: 100%;
   display: flex;
+  min-height: 50%;
   flex-direction: column;
-  padding-left: 20px;
   box-sizing: border-box;
-
 `;
 
 export const ContactDetailButtons = styled.div`
-  width: 40%;
   display: flex;
+  height: 100px;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: space-between;
 `;
 
 export const EditContact = styled(Link)`
   text-decoration: none;
-  width: 150px;
-  height: 50px;
+  width: 120px;
+  height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #0A7EE3;
+  background-color: #0a7ee3;
   border-radius: 6px;
   color: white;
-  font-size: 20px;
+  font-size: 15px;
 `;
 
 export const DeleteContact = styled.button`
   text-decoration: none;
-  width: 150px;
-  height: 50px;
+  width: 120px;
+  height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #DB1414;
+  background-color: #db1414;
   border-radius: 6px;
   color: white;
-  font-size: 20px;
+  font-size: 15px;
   border: none;
   cursor: pointer;
 `;
